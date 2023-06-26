66e92732d9227eea46ccec9cca1a63432d70ca1d
José Santos <josesantos@146-193-190-219.edr.inesc.pt>
2023-06-26 12:28:07 +0100

    meeting 26 of June

--- lib/caching/normalize.ml
+++ lib/caching/normalize.ml
@@ -12,6 +12,23 @@ let rename_var (tbl : (string, string) Hashtbl.t) (x : string) (t : expr_type): 
       Hashtbl.add tbl x x';
       x' 
 
+
+let normalize_relop (op : relop) (e1 : Expression.t) (e2: Expression.t) : Expression.t = 
+  match op with 
+  | Types.Int iop -> 
+    (match iop with 
+    | Gt -> 
+      (* e1 > e2 -> e2 < e1 -> -e1 + e2 + 1 <= 0 *)
+      Printf.printf "Int Eq in my hands!\n"; 
+      assert false 
+    | _ -> 
+      assert false) 
+  | Types.Bool _ -> 
+      assert false 
+  | _ -> Relop (op, e1, e2)
+
+
+
 let rec normalize_aux (tbl : (string, string) Hashtbl.t) (e : Expression.t) : Expression.t = 
   let f = normalize_aux tbl in 
   match e with 
@@ -19,7 +36,7 @@ let rec normalize_aux (tbl : (string, string) Hashtbl.t) (e : Expression.t) : Ex
   | SymPtr (t, e) -> SymPtr (t, f e)
   | Binop (op, e1, e2) -> Binop (op, f e1, f e2)
   | Unop  (op, e) -> Unop (op, f e)
-  | Relop (op, e1, e2) -> Relop (op, f e1, f e2) 
+  | Relop (op, e1, e2) -> normalize_relop op (f e1) (f e2) 
   | Cvtop (op, e) -> Cvtop (op, f e) 
   | Symbolic (t, x) -> Symbolic (t, rename_var tbl x t)
   | Extract (e, t1, t2) -> Extract (f e, t1, t2)
