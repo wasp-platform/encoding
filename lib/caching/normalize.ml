53c310f9e38014384e6533abc430774c76e9ee15
afonso.jose@tecnico.ulisboa.pt <afonso.diamantino2@gmail.com>
2023-07-06 09:43:24 +0100

    Revert "Normalize"
    This reverts commit 70809ebc598129807b11296545280efc430efd2a.

--- lib/caching/normalize.ml
+++ lib/caching/normalize.ml
@@ -19,24 +19,14 @@ let normalize_relop (op : relop) (e1 : Expression.t) (e2: Expression.t) : Expres
     (match iop with 
     | Gt -> 
       (* e1 > e2 -> e2 < e1 -> -e1 + e2 + 1 <= 0 *)
-      let e' = Expression.Binop(Int Add, e2, Val (Int 1)) in
-      let e'' = Expression.Binop(Int Sub, e', e1) in
-      Relop (Int Le, e'', Val (Int 0));
-    | Lt -> 
-      (* e1 < e2 -> e1 - e2 < 0 -> e1 - e2 + 1 <= 0 *)
-      let e' = Expression.Binop(Int Add, e2, Val (Int 1)) in
-      let e'' = Expression.Binop(Int Sub, e1, e') in
-      Relop (Int Le, e'', Val (Int 0));
-    | Ge -> 
-      (* e1 >= e2 -> e1 - e2 <= 0 *)
-      let e' = Expression.Binop(Int Sub, e1, e2) in
-      Relop (Int Le, e', Val (Int 0));
-    | _ -> Relop (op, e1, e2);
-      (* Le, Eq, Ne -> stay the same *)
-    ) 
+      Printf.printf "Int Eq in my hands!\n"; 
+      assert false 
+    | _ -> 
+      assert false) 
   | Types.Bool _ -> 
       assert false 
   | _ -> Relop (op, e1, e2)
+
 
 
 let rec normalize_aux (tbl : (string, string) Hashtbl.t) (e : Expression.t) : Expression.t = 
@@ -51,6 +41,24 @@ let rec normalize_aux (tbl : (string, string) Hashtbl.t) (e : Expression.t) : Ex
   | Symbolic (t, x) -> Symbolic (t, rename_var tbl x t)
   | Extract (e, t1, t2) -> Extract (f e, t1, t2)
   | Concat (e1, e2) -> Concat (f e1, f e2)
+
+(*
+let rec simplify_expression (e : Expression.t) : Expression.t =
+  let f  = simplify_expression in
+  match e with
+  | Val v -> Val v
+  | SymPtr (t, e) -> SymPtr (t, f e)
+  | Binop (op, e1, e2) -> Binop (op, f e1, f e2)
+  | Unop  (op, e) -> Unop (op, f e)
+  | Cvtop (op, e) -> Cvtop (op, f e) 
+  | Symbolic (t, x) -> Symbolic (t, x)
+  | Extract (e, t1, t2) -> Extract (f e, t1, t2)
+  | Concat (e1, e2) -> Concat (f e1, f e2)
+  | Relop (op, e1, e2) -> 
+    match e2 with
+    | Val (Int 0) -> Relop (op, f e1, e2)
+    | _ -> Relop (op, Binop ( (type_of op) Sub, f e1, f e2), Val (Int 0));;
+*)
 
 let normalize (e : Expression.t) : Expression.t = 
   let tbl = Hashtbl.create 0 in 
